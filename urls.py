b280870b1b071236253ad6cbbd3190e0552e9e1c
Goddy21 <116700472+Goddy21@users.noreply.github.com>
2024-04-15 20:29:53 +0300

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+from rest_framework.authtoken import views as rest_framework_views
+#from django.conf.urls import url
+from django.urls import re_path as url
+
+urlpatterns = [
+    path('images/', views.post_alert, name='post_alert'),
+    url(r'^get_auth_token/$', rest_framework_views.obtain_auth_token, name='get_auth_token'),
+]
