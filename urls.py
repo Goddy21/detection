4ce031a158b09414d7ba99fa7e8323e96e947b8a
Goddy21 <116700472+Goddy21@users.noreply.github.com>
2024-04-15 20:32:51 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,23 +1,26 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
+"""
+URL configuration for wd_ss project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+#from django.conf.urls import url
+from django.urls import re_path as url
 
 urlpatterns = [
-    path("login/", views.loginPage, name="login"),
-    path("register/", views.registerPage, name="register"),
-    path("logout/", views.logoutUser, name="logout"),
-    path("", views.home, name="home"),
-    path("reset_password/",
-         auth_views.PasswordResetView.as_view(template_name="detection/password_reset.html"),
-         name="reset_password"),
-    path("reset_password_sent/",
-         auth_views.PasswordResetDoneView.as_view(template_name="detection/password_reset_sent.html"),
-         name="password_reset_done"),
-    path("reset/<uidb64>/<token>/",
-         auth_views.PasswordResetConfirmView.as_view(template_name="detection/password_reset_form.html"),
-         name="password_reset_confirm"),
-    path("reset_password_complete/",
-         auth_views.PasswordResetCompleteView.as_view(template_name="detection/password_reset_done.html"),
-         name="password_reset_complete"),
-    path('alert/<uuid:pk>/', views.alert, name='alert'),
+    path('admin/', admin.site.urls),
+    path('', include("detection.urls")),
+    url(r'^api/', include(('alertupload_rest.urls', 'alertupload_rest'), namespace='api')),
 ]
